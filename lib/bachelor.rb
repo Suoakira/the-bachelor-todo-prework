7a362db8151c09336b2ea6a67eda58b736c57428
Stephen Courtier <steves0089@gmail.com>
2018-09-16 15:20:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,7 +51,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  age_total = 0 
+  age_total.to_f = 0 
   contestent_count = 0 
   data.each do |season, people_array|
     people_array.each do |contestent|

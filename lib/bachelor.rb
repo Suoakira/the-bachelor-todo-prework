8fb852abe1124fc36931afc731872dd7d3f3307f
Stephen Courtier <steves0089@gmail.com>
2018-09-16 15:06:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,68 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+    people.each do |key, value|
+      if value == "Winner"
+        winner = people["name"].split(" ")
+        return winner[0]
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, people_array|
+    people_array.each do |contestent|
+      contestent.each do |key, value|
+      if value == occupation
+        name = contestent["name"]
+        return name
+      end
+    end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+  data.each do |season, people_array|
+    people_array.each do |contestent|
+      contestent.each do |key, value|
+        if value == hometown
+          counter += 1 
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, people_array|
+    people_array.each do |contestent|
+      contestent.find do |key, value| # use find as stops after first found
+        if value == hometown
+          return contestent["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0 
+  contestent_count = 0 
+  data.each do |season, people_array|
+    people_array.each do |contestent|
+      contestent.each do |key, value|
+        if key == "age"
+          age_total += contestent["age"][value].to_f
+          contestent_count += 1 
+        end
+      end
+    end
+  end
+  average_contestent_age = age_total / contestent_count
+  average_contestent_age.round
 end
